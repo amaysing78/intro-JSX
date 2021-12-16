9add5c0193d7a7b009c131d2d6a1508e2eb3d385
amaysing78 <34718110+amaysing78@users.noreply.github.com>
2021-12-16 10:34:04 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favorite drinks.</h1>
+    <ul>
+      <li>Beer</li>
+      <li>Whiskey Sour</li>
+      <li>Wine</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
